84a517768d311920afbc4d370e951f4a9c7ca0fb
zhenya <choshina@hotmail.com>
2021-03-10 17:22:53 +0900

    breach sampling

--- lib/randomSample.m
+++ lib/randomSample.m
@@ -1,30 +1,12 @@
 function [x_log, obj_log] = randomSample(br, budget, phi, cp, tspan, input_name, input_range)
     
-
-
-   % InitBreach;
-
-   % mdl = 'Autotrans_shift';
-   % Br = BreachSimulinkSystem(mdl);
-
     br.Sys.tspan = tspan;
 
     input_gen.type = 'UniStep';
-    %input_gen.cp = input('\n Please input control points\n');
     input_gen.cp = cp;
     br.SetInputGen(input_gen);
-
-
     rng('default')
     rng('shuffle')
-    
-
-    
-
-
-    %num_sim = input('\n Please input the number of simulations: \n');
-
-    %log = [];
     x_log = [];
     obj_log = [];
 
@@ -33,9 +15,6 @@ function [x_log, obj_log] = randomSample(br, budget, phi, cp, tspan, input_name,
         t_x_log = [];
         t_obj_log = [];
         for i = 1: budget
-
-    %        x1 = (r1(2)-r1(1))*rand + r1(1);
-    %        x2 = (r2(2)-r2(1))*rand + r2(1);
             x_list = [];
             [dim, ~] = size(input_range);
             for j = 1:dim
