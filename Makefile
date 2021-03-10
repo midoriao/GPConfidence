d8bdec127d09f3d287ef0a6d9fffbf2655020795
choshina <choshina@hotmail.com>
2021-03-11 01:57:32 +0900

    logging

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
-SH=$(wildcard test/benchmark/*)
-CSV=$(SH:test/benchmark/%=results/%.csv)
+SH=$(wildcard test/benchmarks/*)
+CSV=$(SH:test/benchmarks/%=results/%.csv)
 DIRS=results output
 
 .PHONY: all scalac
 
 all: $(DIRS) $(CSV)
-	# git commit -m "new benchmark results"
 
 results:
 	mkdir -p $@
@@ -13,16 +12,8 @@ results:
 output:
 	mkdir -p $@
 
-results/%.csv: test/benchmark/%
+results/%.csv: test/benchmarks/%
 	./run $* $< $@
-	# git add $@
 	
-results/at-breach-cmaes.csv: src/test/matlab/AT.m
-
-scalac:
-	scalac -d bin -cp lib/engine.jar `find src/main/scala src/test/scala/hybrid/benchmarks -iname "*.scala"`
-
-benchmarks.jar: Manifest.txt
-	jar cfm $@ Manifest.txt -C bin .
 clean:
-	rm *.dat *.mat *.slxc *mex*
+	rm *.dat *.slxc *mex*
--- test/breach_test.py
+++ test/breach_test.py
@@ -82,7 +82,7 @@ for ph in phi_str:
 	for cp in controlpoints:
 		for opt in optimization:
 			property = ph.split(';')
-			ts = time.strftime('%Y%m%d-%H%M',time.localtime(time.time()))
+			ts = time.strftime('%Y%m%d%H%M',time.localtime(time.time()))
 			filename = model+  '_' + property[0] + '_' + str(cp)  +'_' + opt + '_' + str(max_sim)
 
 			param = '\n'.join(parameters)
@@ -139,7 +139,7 @@ for ph in phi_str:
 					bm.write('\tlogs.obj_log = tlogs[1:ns];\n')
 					bm.write('\tsave(filename, \'logs\', \'vars\', \'ranges\');\n')
 				else:
-					bm.write('\tfilename = strcmp(\'log/' + dirname + '/\', \''  + filename + '\',\'_\', int2str(budget) , \'_\', int2str(n) , \'_\', \'' + ts + '\');\n')
+					bm.write('\tfilename = strcmp(\'log/' + dirname + '/\', \''  + filename + '\',\'_\', int2str(budget) , \'_\', int2str(n) , \'_\', \'' + ts + '.mat\');\n')
 					bm.write('\tsave(filename, \'logs\', \'vars\', \'ranges\');\n')
 				bm.write('end\n')
 				
