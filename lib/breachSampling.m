ec11b3b8449312965c6e076ad9e701ae2e6b847d
zhenya <choshina@hotmail.com>
2021-03-10 18:03:28 +0900

    sampling

--- lib/breachSampling.m
+++ lib/breachSampling.m
@@ -1,29 +1,13 @@
-
 %br, budget, phi, cp, tspan, input_name, input_range, solver
-function [x_log,obj_log] = breachSampling(br, budget, phi, cp, tspan, input_name, input_range, solver)
-
-    %InitBreach;
-
-    %mdl = 'Autotrans_shift';
-    %Br = BreachSimulinkSystem(mdl);
+function [logs, vars, ranges] = breachSampling(br, budget, phi, cp, tspan, input_name, input_range, solver)
 
     br.Sys.tspan = tspan;
 
     input_gen.type = 'UniStep';
     input_gen.cp = cp;
     br.SetInputGen(input_gen);
-   
-
-
-    %for cpi = 0:input_gen.cp-1
-    %    throttle_sig = strcat('throttle_u', num2str(cpi));
-    %    brake_sig = strcat('brake_u', num2str(cpi));
-    %    Br.SetParamRanges({throttle_sig},[0 100]);
-    %    Br.SetParamRanges({brake_sig},[0 325]);
-    %end
     
     dim = numel(input_name);
-   
     
     for i = 1:cp
         for j = 1:dim
@@ -32,33 +16,32 @@ function [x_log,obj_log] = breachSampling(br, budget, phi, cp, tspan, input_name
         end
     end
 
-    
-    x_log = [];
-    obj_log = [];
-    
-    while true
+    logs = [];
+    logs.X_log = [];
+    logs.obj_log = [];
 
-        falsif_pb = FalsificationProblem(br, phi);
-        falsif_pb.max_time = Inf;
-        falsif_pb.max_obj_eval = budget;
-        falsif_pb.setup_solver(solver);
-        falsif_pb.solve();
-        if any(falsif_pb.obj_log<0)
-            continue;
-        else
-            break;
-        end
-       
-    end
-    
-    
-    x_log = falsif_pb.X_log;
-    obj_log = falsif_pb.obj_log;
-    
-    
+	while true
+    	falsif_pb = FalsificationProblem(br, phi);
+    	falsif_pb.max_time = Inf;
+    	falsif_pb.max_obj_eval = budget;
+		
+		if contains(solver, '_')
+			s = split(solver, '_');
+			falsif_pb.setup_solver(s{1});
+			falsif_pb.solver_options.PopSize = str2num(s{2});
+		else
+			falsif_pb.setup_solver(solver);
+		end
 
-    %save('test.mat', 'falsif_pb');
+    	falsif_pb.solve();
+		if falsif_pb.best_obj > 0
+			break	
+		end
+	end
 
-
+    logs.X_log = falsif_pb.X_log;
+    logs.obj_log = falsif_pb.obj_log;
+	vars = br.GetSysVariables();
+	ranges = br.GetParamRanges(vars);
 
 end
--- lib/randomSample.m
+++ lib/randomSample.m
@@ -1,60 +1,46 @@
-function [x_log, obj_log] = randomSample(br, budget, phi, cp, tspan, input_name, input_range)
-    
+function [logs, vars, ranges] = randomSample(br, budget, phi, cp, tspan, input_name, input_range)
     br.Sys.tspan = tspan;
 
     input_gen.type = 'UniStep';
     input_gen.cp = cp;
     br.SetInputGen(input_gen);
+
+
     rng('default')
     rng('shuffle')
-    x_log = [];
-    obj_log = [];
+    
+    logs = [];
+    logs.X_log = [];
+    logs.obj_log = [];
 
-    while true
+    for i = 1: budget
+
+        x_list = [];
+        [dim, ~] = size(input_range);
+        for j = 1:dim
+            for h = 1:cp
+                x_temp = (input_range(j, 2) - input_range(j, 1))*rand + input_range(j, 1);
+                x_list = [x_list x_temp];
+            end
+        end
         
-        t_x_log = [];
-        t_obj_log = [];
-        for i = 1: budget
-            x_list = [];
-            [dim, ~] = size(input_range);
+        
+        for cpi = 1:cp
             for j = 1:dim
-                for h = 1:cp
-                    x_temp = (input_range(j, 2) - input_range(j, 1))*rand + input_range(j, 1);
-                    x_list = [x_list x_temp];
-                end
+                br.SetParam({strcat(input_name(j),'_u',num2str(cpi-1))}, x_list((j-1)*cp + cpi))
             end
-
-
-            for cpi = 1:cp
-                for j = 1:dim
-                    br.SetParam({strcat(input_name(j),'_u',num2str(cpi-1))}, x_list((j-1)*cp + cpi))
-                end
-            end
-
-
-            t_x_log = [t_x_log x_list'];
-
-
-            br.Sim(0:.01:30);
-            obj = br.CheckSpec(phi);
-            if obj < 0
-                break;
-            end
-            
-            t_obj_log = [t_obj_log obj];
-
         end
-        if numel(obj_log) < budget
-            continue;
-        else
-            x_log = t_x_log;
-            obj_log = t_obj_log;
-            break;
-        end
+
         
-        
+        logs.X_log = [logs.X_log x_list'];
+
+
+        br.Sim(0:.01:30);
+        obj = br.CheckSpec(phi)
+        logs.obj_log = [logs.obj_log obj];
+		vars = br.GetSysVariables();
+		ranges = br.GetParamRanges(vars);
+
     end
 
-    %save('test.mat', 'falsif_pb');
-    %object = min(falsif_pb.obj_log);
 end
