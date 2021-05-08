591cf68beb83f80d5f9016837cf7ef001d6cf800
zhenya <choshina@hotmail.com>
2021-05-08 11:35:51 +0900

    br

--- lib/breachSampling.m
+++ lib/breachSampling.m
@@ -19,25 +19,35 @@ function [logs, vars, ranges] = breachSampling(br, budget, phi, cp, tspan, input
     logs = [];
     logs.X_log = [];
     logs.obj_log = [];
-    for ii = 1:10
+    
+    rem_budget = budget;
+    while true
     	falsif_pb = FalsificationProblem(br, phi);
     	falsif_pb.max_time = Inf;
-    	falsif_pb.max_obj_eval = budget;
+    	falsif_pb.max_obj_eval = rem_budget;
 		
-	if contains(solver, '_')
-		s = split(solver, '_');
-		falsif_pb.setup_solver(s{1});
-		falsif_pb.solver_options.PopSize = str2num(s{2});
-	else
-		falsif_pb.setup_solver(solver);
-	end
+        if contains(solver, '_')
+            s = split(solver, '_');
+            falsif_pb.setup_solver(s{1});
+            falsif_pb.solver_options.PopSize = str2num(s{2});
+        else
+            falsif_pb.setup_solver(solver);
+        end
 
-    	falsif_pb.solve();
-	if min(falsif_pb.obj_log) > 0 && numel(falsif_pb.obj_log) >= budget
-		logs.X_log = falsif_pb.X_log;
-		logs.obj_log = falsif_pb.obj_log;
-		break;
-	end
+        falsif_pb.solve();
+        if min(falsif_pb.obj_log) > 0  %&& numel(falsif_pb.obj_log) >= budget
+            logs.X_log = [logs.X_log falsif_pb.X_log];
+            logs.obj_log = [logs.obj_log falsif_pb.obj_log];
+        else
+            continue;
+        end
+        
+        if numel(falsif_pb.obj_log) >= rem_budget
+            break;
+        else
+            rem_budget = rem_budget - numel(falsif_pb.obj_log);
+        end
+        
     end
 
 
